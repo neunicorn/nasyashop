6d60d6251370f139002cf8c8c2a4a00a7fd84571
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2025-01-04 13:26:39 +0700

    task 21 -  make cart and cart item repository

---
+++ src/main/java/com/nasya/ecommerce/repository/CartItemRepository.java
@@ -0,0 +1,35 @@
+package com.nasya.ecommerce.repository;
+
+import com.nasya.ecommerce.entity.Cart;
+import com.nasya.ecommerce.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query(value = """
+    SELECT ci.* FROM cart_items ci
+        JOIN carts c ON c.cart_id = ci.cart_id
+        WHERE c.user_id = :userId
+    """, nativeQuery = true)
+    List<CartItem> getUserCartItems(Long userId);
+
+    @Query(value = """
+    SELECT * FROM cart_items
+        WHERE cart_id = :cartId
+        AND product_id = :productId
+    """, nativeQuery = true)
+    CartItem findByCartIdAndProductId(Long cartId, Long productId);
+
+    @Modifying
+    @Query(value = """
+    DELETE FROM cart_items
+        WHERE cart_id = :cartId
+    """, nativeQuery = true)
+    void deleteAllByCartId(Long cartId);
+}
